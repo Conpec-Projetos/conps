68560836a7beda0595464d4692f8bde2d199123b
yellowisk <oheitoralmeida2013@gmail.com>
2024-12-22 17:06:03 -0300

    setting up stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,47 @@
 import type { Config } from "tailwindcss";
 
+import { nextui } from "@nextui-org/react";
+
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./context/**/*.{ts,tsx}",
+    "./src/assets/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ["var(--font-roboto)"],
+        poppins: ["var(--font-poppins)"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      dropShadow: {
+        white: [
+          "0 0px 20px rgba(255,255, 255, 0.45)",
+          "0 0px 65px rgba(255, 255,255, 0.4)",
+        ],
+      },
+      caretColor: {
+        transparent: "transparent",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: "rgba(60, 47, 47, 1)",
+            secondary: "#6A6A6A",
+          },
+        },
+      },
+    }),
+  ],
 } satisfies Config;
