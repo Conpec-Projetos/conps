4ed523823243536b83ce63c252a81fa25c661468
yellowisk <oheitoralmeida2013@gmail.com>
2025-02-01 02:54:45 -0300

    perf: improve image handling

--- src/components/ui/conpec-bar.tsx
+++ src/components/ui/conpec-bar.tsx
@@ -4,7 +4,7 @@ import LogoStylized from "@assets/conpec-styled.png";
 export const ConpecBar = () => {
     return (
         <div className="flex flex-col items-center justify-center w-full lg:h-auto lg:w-[25%] bg-orange-700">
-        <Image src={LogoStylized} alt="Conpec" width={250} height={250} />
+        <Image priority quality={100} src={LogoStylized} alt="Conpec" width={250} height={250} />
       </div>  
     )
 }
